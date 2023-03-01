0120db7d70ae84cac6aec9801b926b0a67378fae
Shrikrushna <shrikrushnamantalwad@gmail.com>
2023-03-01 23:46:49 +0530

    set view engine and views

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 8000;
 //use express router
 app.use("/", require("./routes/index"));
 
-//set up the view engine
+//set up the view engine and views
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
